b5555ed5a7f572f3ec23390c274f70c3ffafea6d
Jose <joseeenrique.jm@gmail.com>
2022-11-07 18:49:20 -0700

    working on script file

---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
